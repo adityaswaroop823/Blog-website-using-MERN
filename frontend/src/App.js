4ba383513c22e09ac164543c6a9863e2d7d8fa76
Aditya Swaroop <adityaswaroop80@gmail.com>
2024-02-20 02:24:57 +0530

    Added routes,footer and header

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import Nav from './components/Nav';
+import './App.css';
+import Footer from './components/Footer';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+function App() {
+  return (
+    <div className="App">
+    <BrowserRouter>
+   
+      <Nav/>
+ <   Routes>
+ <Route path="/" element={<h1>Product Listing Component</h1>}></Route>
+ <Route path="/add" element={<h1>Add Product Component</h1>}></Route>
+ <Route path="/update" element={<h1>Update Product Component</h1>}></Route>
+ <Route path="/logout" element={<h1>Logout Component</h1>}></Route>
+ <Route path="/profile" element={<h1>User Profile Component</h1>}></Route>
+ </Routes>
+
+
+    </BrowserRouter>
+   <div className='footer-div'>
+    <Footer/>
+    </div>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import react from "react";
+const Footer=()=>{
+    return(
+        <div>
+            <h3>
+                Mern Project
+            </h3>
+        </div>
+    )
+}
+export default Footer;
